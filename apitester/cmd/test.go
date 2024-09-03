d6c8d84506bf5f8ddd9b572e48b89a09b0930ac5
Trend20 <enockomondi305@gmail.com>
2024-09-03 13:01:34 +0300

    adding flags to the testCmd command

--- apitester/cmd/test.go
+++ apitester/cmd/test.go
@@ -4,13 +4,24 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// command flags
+var url string
+var body string
+var method string
+var headers []string
+
+// actual command body
 var testCmd = &cobra.Command{
-	Use:   "apitester",
-	Short: "A CLI tool that tests APIs and generates API documentation.",
-	Long: `A CLI tool that tests APIs and generates API documentation based on the test results.
-Support different formats like OpenAPI/Swagger and include automated testing features.`,
-	Run: func(cmd *cobra.Command, args []string) {},
+	Use:   "test",
+	Short: "Test an  API endpoint.",
+	Long:  `Test a specific API endpoint with specified HTTP method, headers, and body.`,
+	Run:   func(cmd *cobra.Command, args []string) {},
 }
 
 func init() {
+	//	assign the flags to the command
+	testCmd.Flags().StringVarP(&url, "url", "u", "", "API endpoint URL")
+	testCmd.Flags().StringVarP(&method, "method", "m", "GET", "HTTP method")
+	testCmd.Flags().StringArrayVarP(&headers, "header", "H", []string{}, "HTTP header")
+	testCmd.Flags().StringVarP(&body, "body", "b", "", "HTTP Request body")
 }
