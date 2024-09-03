722c563e01885bd16e593b02e39f653bbaf6df9e
Trend20 <enockomondi305@gmail.com>
2024-09-03 12:53:00 +0300

    code cleanup

--- apitester/cmd/test.go
+++ apitester/cmd/test.go
@@ -1,12 +1,10 @@
 package cmd
 
 import (
-	"os"
-
 	"github.com/spf13/cobra"
 )
 
-var rootCmd = &cobra.Command{
+var testCmd = &cobra.Command{
 	Use:   "apitester",
 	Short: "A CLI tool that tests APIs and generates API documentation.",
 	Long: `A CLI tool that tests APIs and generates API documentation based on the test results.
@@ -14,15 +12,5 @@ Support different formats like OpenAPI/Swagger and include automated testing fea
 	Run: func(cmd *cobra.Command, args []string) {},
 }
 
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-}
-
 func init() {
-	// Add additional commands here
-	rootCmd.AddCommand(testCmd)
-	rootCmd.AddCommand(generateDocCmd)
 }
